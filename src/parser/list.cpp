6217827bbda4ce534c9a51560bf5358db66d63e4
Micha <micha@infinitycoding.de>
2016-03-05 21:44:46 +0100

    only pre evaluate 'arguments'

--- src/parser/list.cpp
+++ src/parser/list.cpp
@@ -44,58 +44,50 @@ int parse_list_se(Context* context, vword_t addr, SNode* ast)
     // needed space
     size_t n = ast->subnodes->numOfElements();
 
-	size_t len = 4*VWORD_SIZE;
-	size_t j = 0;
-	vword_t lptr = addr + len;
+    size_t len = 4*VWORD_SIZE;
+    size_t j = 0;
+    vword_t lptr = addr + len;
 
-	len += 2 * n * VWORD_SIZE;
+    len += 2 * n * VWORD_SIZE;
 
     ListIterator<SNode*> it = ListIterator<SNode*>(ast->subnodes);
     while(! it.isLast())
     {
         SNode* sn = it.getCurrent();
-		vword_t subaddr;
+        vword_t subaddr;
+        vword_t m;
 
         switch(sn->type)
         {
             case SYMBOL:
             case INTEGER:
                 // direcly insert word
-				context->write_word(lptr + (j++)*VWORD_SIZE, VWORD_SIZE);
+                context->write_word(lptr + (j++)*VWORD_SIZE, VWORD_SIZE);
                 parse_se(context, lptr + (j++)*VWORD_SIZE, sn);
                 break;
 
             case LIST:
-				subaddr = addr+len;
-				len += parse_se(context, subaddr, sn);
-
-				context->write_word(lptr + (j++)*VWORD_SIZE, (vword_t)-1);
-                context->write_word(lptr + (j++)*VWORD_SIZE, subaddr);
-				break;
-
             case STRING:
-/*                // here we need pointers
-                vword_t subaddr = addr + len;
-                len += parse(context, subaddr, sn);
+                subaddr = addr+len;
+                len += parse_se(context, subaddr, sn);
 
-                context->write_word(addr+len, subaddr);
-				len += VWORD_SIZE;
+                m = (j == 0 || sn->type == STRING) ? VWORD_SIZE : -1;
 
-				list[i++]
-*/
+                context->write_word(lptr + (j++)*VWORD_SIZE, m);
+                context->write_word(lptr + (j++)*VWORD_SIZE, subaddr);
                 break;
         }
 
         it.next();
     }
 
-	vword_t v[4];
-	v[0] = 3 * VWORD_SIZE;
-	v[1] = resolve_symbol("deval");
-	v[2] = n;
-	v[3] = lptr;
+    vword_t v[4];
+    v[0] = 3 * VWORD_SIZE;
+    v[1] = resolve_symbol("deval");
+    v[2] = n;
+    v[3] = lptr;
 
-	context->write(addr, 4*VWORD_SIZE, (vbyte_t*) &v);
+    context->write(addr, 4*VWORD_SIZE, (vbyte_t*) &v);
 
     return len;
 }
