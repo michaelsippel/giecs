93f8fe0537ba63cd39b7339ba9ad366817eaf14b
Micha <micha@infinitycoding.de>
2016-12-11 00:00:52 +0100

    block sync functions for bits-class

--- include/bits.h
+++ include/bits.h
@@ -4,6 +4,8 @@
 #include <boost/preprocessor/repetition/repeat.hpp>
 #include <boost/preprocessor/tuple/elem.hpp>
 
+#include <memory/block.h>
+
 namespace giecs
 {
 
@@ -65,12 +67,21 @@ class Bits
 		{ \
 			return Bits(this->value op v); \
 		} \
+		Bits operator op (Bits const v) const \
+		{ \
+			return Bits(this->value op v.value); \
+		} \
 
 #define OPERATOR_EQ(op) \
 		template <typename T> \
 		Bits& operator op (T const v) \
 		{ \
 			this->value op v; \
+			return *this; \
+		} \
+		Bits& operator op (Bits const v) \
+		{ \
+			this->value op v.value; \
 			return *this; \
 		} \
 
@@ -112,6 +123,95 @@ constexpr size_t bitsize(T)
 {
     return bitsize<T>();
 }
+
+namespace memory
+{
+/*  E X A M P L E
+ *  page_size = 2
+ *  block_size = 2
+ *                          offset = 1 (x align_t)
+ *                          V
+ *          |     buf[0]    |      buf[1]   |
+ *   – – – –|– – – – – – – –|– – – – – – – –|– – – –   buf (align_t)
+ *          |            P A G E            |
+ *  –––––––––––––––––––––––––––––––––––––––––––––––––
+ *  |       B L O C K       |       B L O C K       |
+ *  |– – – – – –|– – – – – –|– – – – – –|– – – – – –|  block (val_t)
+ *  |block[i-1] | block[i]  |  block[0] | block[1]  |
+ *
+ *  ^       ^ boff = 8 bits ^
+ *  |       |_ _ _ _ _ _ _ _|
+ *  |
+ *  |           ^ ioff = 1  ^
+ *  |           | i = 1     |
+ *  |       ^   ^
+ *  |       |_ _| bitoff = 2 bits
+ *  |
+ *  block_id = -1
+ */
+
+template <size_t page_size, int N_align, int N_val>
+void read_block(TypeBlock<page_size, Bits<N_align>, Bits<N_val>> const& block, int off, int bitoff, Bits<N_align>* buf)
+{
+    int i = 0;
+    if(off < 0)
+    {
+        i -= (off * N_align + bitoff) / N_val;
+        off = 0;
+    }
+
+    size_t  block_size = 4;
+    while(off < page_size && i <= block_size)
+    {
+        Bits<N_align> a = Bits<N_align>();
+
+        if(bitoff > 0 && i > 0)
+            a = Bits<N_align>(block[i-1] >> (N_val - bitoff));
+
+        while(bitoff < N_align && i < block_size)
+        {
+            if(bitoff < 0)
+                a = Bits<N_align>(block[i++] >> (-bitoff));
+            else
+                a |= Bits<N_align>(block[i++]) << bitoff;
+            bitoff += N_val;
+        }
+        bitoff -= N_align;
+        buf[off++] |= a;
+    }
+}
+
+template <size_t page_size, int N_align, int N_val>
+void write_block(TypeBlock<page_size, Bits<N_align>, Bits<N_val>> const& block, int off, int bitoff, Bits<N_align> const* buf)
+{
+    int i = 0;
+    if(off < 0)
+    {
+        i -= (off * N_align + bitoff) / N_val;
+        off = 0;
+    }
+
+    size_t block_size = 4;
+    while(off < page_size && i <= block_size)
+    {
+        Bits<N_align> a = buf[off++];
+
+        if(bitoff > 0 && i > 0)
+            block[i-1] |= Bits<N_val>(a) << (N_val - bitoff);
+
+        while(bitoff < N_align && i < block_size)
+        {
+            if(bitoff < 0)
+                block[i++] |= Bits<N_val>(a) << (-bitoff);
+            else
+                block[i++] |= Bits<N_val>(a >> bitoff);
+            bitoff += N_val;
+        }
+        bitoff -= N_align;
+    }
+}
+
+} // namespace memory
 
 } // namespace giecs
 
