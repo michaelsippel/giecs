114f7a6c21069324d1010ba0c30c92b568657662
Michael Sippel <micha@infinitycoding.de>
2017-08-18 00:06:32 +0200

    linear accessor: helper functions for calculating indices

--- giecs/memory/accessors/linear.hpp
+++ giecs/memory/accessors/linear.hpp
@@ -6,6 +6,7 @@
 #include <boost/shared_ptr.hpp>
 
 #include <giecs/memory/accessor.h>
+#include <giecs/memory/reference.hpp>
 #include <giecs/bits.h>
 
 namespace giecs
@@ -21,8 +22,54 @@ class Linear : public Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t
         template <size_t, typename, typename, typename, typename, typename> friend class Linear;
 
     private:
-        // one block is one page
-        static size_t const block_size = (page_size * bitsize<align_t>()) / bitsize<val_t>();
+        static size_t const block_size = 512; // 32bit/val -> 2 MiB.  // TODO: more dynamic
+        static int const nalign = (bitsize<val_t>() > bitsize<align_t>()) ?
+                                  (1+int(bitsize<val_t>()-1)/int(bitsize<align_t>())) :  // positive: number of align-blocks per value
+                                  (-1-int(bitsize<align_t>()-1)/int(bitsize<val_t>())); // negative: number of values per align-block
+
+        // align per value
+        static inline int alignOffset(int const nval)
+        {
+            return (nalign > 0) ? (nval * nalign) : (nval / (-nalign));
+        }
+        // value per align
+        static inline int valueOffset(int const na)
+        {
+            return (nalign > 0) ? (na / nalign) : (na * (-nalign));
+        }
+        // pages per value
+        static inline int pageOffset(int const nval)
+        {
+            return alignOffset(nval) / page_size;
+        }
+        // blocks per value
+        static inline int blockOffset(int const nval)
+        {
+            return nval / block_size;
+        }
+        // align offset in value
+        static inline int alignShift(Reference const ref)
+        {
+            // TODO: fix possible integer overflow
+            return (nalign < 1) ? 0 : ((ref.page_id * page_size + ref.offset) % nalign);
+        }
+        inline unsigned int alignShift() const
+        {
+            return alignShift(this->reference);
+        }
+
+        inline index_t valueIndex(addr_t const addr) const
+        {
+            return index_t((valueOffset(this->reference.page_id*page_size+this->reference.offset) + (unsigned int)addr) % block_size);
+        }
+        inline unsigned int pageIndex(addr_t const addr) const
+        {
+            return this->reference.page_id + pageOffset((unsigned int) addr);
+        }
+        inline unsigned int blockIndex(addr_t const addr) const
+        {
+            return blockOffset(valueOffset(this->reference.page_id*page_size+this->reference.offset) + (unsigned int)addr);
+        }
 
         using typename ContextSync<page_size, align_t>::BlockPtr;
         using typename ContextSync<page_size, align_t>::BlockRef;
@@ -30,68 +77,58 @@ class Linear : public Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t
 
     public:
 #define TYPEID boost::typeindex::type_id< Linear<page_size, align_t, addr_t, val_t, buf_t, index_t> >()
-#define ID(flags) {TYPEID, size_t( flags )}
+#define ID_F(flags) {TYPEID, size_t( flags )}
+#define ID(ref) ID_F( this->alignShift(ref) )
+
 
         Linear(Context<page_size, align_t> const& context_)
             : Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t>::Accessor(context_, ID(0))
-        {
-            this->offset = index_t();
-        }
-
-        Linear(Context<page_size, align_t> const& context_, index_t offset_)
-            : Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t>::Accessor(context_, ID(offset_)), offset(offset_)
         {}
+
+        Linear(Context<page_size, align_t> const& context_, Reference ref_)
+            : Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t>::Accessor(context_, ID(ref_)), reference(ref_)
+        {
+            std::cout << "create linear at " << reference.page_id << " " << reference.offset << "nalign:" << nalign << ": blockid " << blockIndex(addr_t()) << " , index " << valueIndex(addr_t()) << std::endl;
+        }
 
         template <typename addr2_t, typename val2_t, typename buf2_t, typename index2_t>
         Linear(Linear<page_size, align_t, addr2_t, val2_t, buf2_t, index2_t> const& l)
-            : Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t>::Accessor(l.context, ID(l.offset)), offset(l.offset)
+            : Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t>::Accessor(l.context, ID(l.reference)), reference(l.reference)
         {}
-
-        template <typename addr2_t, typename val2_t, typename buf2_t, typename index2_t>
-        Linear(Linear<page_size, align_t, addr2_t, val2_t, buf2_t, index2_t> const& l, int off)
-            : Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t>::Accessor(l.context, ID(l.offset+off)), offset(l.offset+off)
-        {
-        }
-
+        /*
+              template <typename addr2_t, typename val2_t, typename buf2_t, typename index2_t>
+              Linear(Linear<page_size, align_t, addr2_t, val2_t, buf2_t, index2_t> const& l, int off)
+              : Accessor<>((l.offset+off), offset(l.offset+off))
+              {
+              }
+        */
 #undef TYPEID
 #undef ID
 
-        int getOffset(void) const
+        size_t operate(addr_t const oaddr, index_t const len, std::function<void (val_t&)> const operation, bool dirty) const
         {
-            return offset;
-        }
-
-        index_t operate(addr_t const addr, index_t const len, std::function<void (val_t&)> const operation, bool dirty) const
-        {
-            index_t l;
-
+            size_t l;
+            addr_t addr = oaddr;
             if(operation)
             {
-                int const start = int(addr);
-                int const end = int(addr+len);
-
-                unsigned int page_id = (this->offset * bitsize<align_t>() + start * bitsize<val_t>()) / bitsize<align_t>() / page_size;
-                unsigned int block_id = start / block_size;
-                unsigned int nblocks = 1+(end-start)/block_size;
-
-                index_t i = index_t(start % block_size);
-                index_t last_i = index_t(end % block_size);
-
-                if(last_i == index_t())
-                    last_i = block_size;
-
-                for(int n=1; n <= nblocks; ++n)
+                addr_t const end = addr + len;
+                unsigned int block_id, page_id;
+                unsigned int const last_block = this->blockIndex(end);
+                do
                 {
-                    TypeBlockPtr block = this->getBlock(page_id++, block_id++, dirty);
-                    index_t end = (n == nblocks) ? last_i : index_t(block_size);
-                    for(; i < end; ++i, ++l)
+                    page_id = this->pageIndex(addr);
+                    block_id = this->blockIndex(addr);
+                    TypeBlockPtr block = this->getBlock(page_id, block_id, dirty);
+
+                    index_t i = valueIndex(addr);
+                    for(; (i != block_size) && (addr < end); ++i, ++l, ++addr)
                     {
                         val_t& v = (*block)[i];
                         operation(v);
                     }
 
-                    i = 0;
                 }
+                while(block_id != last_block);
             }
 
             return l;
@@ -151,6 +188,7 @@ class Linear : public Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t
     private:
         // offset in size of align_t
         int offset; // TODO: makes copy/move cheap? only page_id's and sync offsets have to be recalculated
+        Reference reference;
 
         TypeBlockPtr getBlock(unsigned int page_id, unsigned int block_id, bool dirty) const
         {
@@ -161,10 +199,10 @@ class Linear : public Accessor<page_size, align_t, addr_t, val_t, buf_t, index_t
             TypeBlockPtr block = boost::static_pointer_cast<TypeBlock<page_size, align_t, val_t> const>(this->context.getBlock(key));
             if(block == NULL)
             {
-                index_t const off = this->offset;
-                auto const createSync = [off](Context<page_size, align_t> const& c)
+                Reference const ref = this->reference;
+                auto const createSync = [ref](Context<page_size, align_t> const& c)
                 {
-                    return new Linear<page_size, align_t, addr_t, val_t, buf_t, index_t>(c, off);
+                    return new Linear<page_size, align_t, addr_t, val_t, buf_t, index_t>(c, ref);
                 };
                 block = TypeBlockPtr(new TypeBlock<page_size, align_t, val_t>(block_size, createSync));
                 this->context.addBlock(block, key);
--- giecs/memory/reference.hpp
+++ giecs/memory/reference.hpp
@@ -1,29 +1,16 @@
 
 #pragma once
 
-#include <vector>
-
-#include <giecs/memory/context.h>
-
 namespace giecs
 {
 
 namespace memory
 {
 
-template <size_t page_size, typename align_t>
-class Reference
+struct Reference
 {
-    public:
-        Reference(Context<page_size, align_t> const& context_)
-            : context(context_)
-        {
-        }
-
-  unsigned int page_id;
-  unsigned int align_offset;
-
-  Context<page_size, align_t> const& context;
+    unsigned int page_id;
+    unsigned int offset;
 };
 
 } // namespace memory
