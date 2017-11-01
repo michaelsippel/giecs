e3d4dddc9a2eebfb2989e217748686cd18bd67df
Michael Sippel <micha@infinitycoding.de>
2017-11-01 23:33:57 +0100

    ll: add logic & bitwise operations

--- giecs/ll/arithmetic.hpp
+++ giecs/ll/arithmetic.hpp
@@ -9,40 +9,65 @@ namespace giecs
 namespace ll
 {
 
+#define OP(name, op)                            \
+  LLFN( name )                                  \
+  {                                             \
+    val_t a = stack.top();                      \
+    stack.pop();                                \
+    val_t b = stack.top();                      \
+    stack.pop();                                \
+    cast_t r = cast_t(a) op cast_t(b);          \
+    stack.push(val_t(r));                       \
+  }
+
+
+#define SINGLE_OP(name, op)                     \
+  LLFN( name )                                  \
+  {                                             \
+    val_t a = stack.top();                      \
+    stack.pop();                                \
+    cast_t r = cast_t(a);                       \
+    stack.push(val_t(op r));                    \
+  }
+
+
 template <typename cast_t>
 struct Arithmetic
 {
-
-#define OP(name, op) \
-    LLFN( name ) \
-    { \
-        val_t a = stack.top(); \
-        stack.pop(); \
-        val_t b = stack.top(); \
-        stack.pop(); \
-        cast_t r = cast_t(a) op cast_t(b); \
-        stack.push(val_t(r)); \
-    }
-
     OP(add, +)
     OP(sub, -)
     OP(mul, *)
     OP(div, /)
-#undef OP
-
-#define SINGLE_OP(name, op) \
-    LLFN( name ) \
-    { \
-        val_t a = stack.top(); \
-        stack.pop(); \
-        cast_t r = cast_t(a); \
-        op r; \
-        stack.push(val_t(r)); \
-    }
 
     SINGLE_OP(inc, ++)
     SINGLE_OP(dec, --)
+}; // struct Arithmetic
 
+  template <typename cast_t=bool>
+struct Logic
+{
+    SINGLE_OP(op_not, not);
+    OP(op_and, and);
+    OP(op_or, or);
+}; // struct Logic
+
+template <typename cast_t>
+struct Bitwise
+{
+    SINGLE_OP(op_not, compl);
+    OP(op_and, bitand);
+    OP(op_or, bitor);
+    OP(op_xor, xor);
+    OP(leftshift, <<);
+    OP(rightshift, >>);
+}; // struct Bitwise
+
+#undef OP
+#undef SINGLE_OP
+
+template <typename cast_t>
+struct Relation
+{
 #define COMP(name, op) \
     LLFN( name ) \
     { \
@@ -61,8 +86,7 @@ struct Arithmetic
     COMP(get, >=)
     COMP(let, <=)
 #undef COMP
-
-};
+}; // struct Relation
 
 } // namespace ll
 
